852361775dd277a8129439c86fc6002e05da9354
tosiq ahmed <m.tosiqshafiq@yahoo.com>
2021-03-26 01:59:01 +0500

    data controller view

---
+++ WebApplication19/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication19.Models;
+namespace WebApplication19.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var t = new List<Student>();
+            var user1 = new Student();
+            user1.id = 1;
+            user1.name = "tosiq";
+            user1.password = "123";
+            user1.age = 20;
+
+            var user2 = new Student();
+            user2.id = 2;
+            user2.name = "laiq";
+            user2.password = "123";
+            user2.age = 21;
+
+            var user3 = new Student();
+            user3.id = 3;
+            user3.name = "saneeya";
+            user3.password = "123";
+            user3.age = 22;
+
+            t.Add(user1);
+            t.Add(user2);
+            t.Add(user3);
+            ViewBag.a = t;
+
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication19/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication19.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string password { get; set; }
+        public int age { get; set; }
+    }
+}
